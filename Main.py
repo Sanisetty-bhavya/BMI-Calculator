3cf70d8a3fff75c10655abbf3927e86d849831c1
Sanisetty-bhavya <114378144+Sanisetty-bhavya@users.noreply.github.com>
2024-01-19 10:12:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,3 @@
-from tabulate import tabulate #for printing the Details and BMi in tabular form
-from sys import exit #for 3rd case to exit the program
-import random #for randint function
-#import sys
-#import math
-#import os
-#import smtplib
-from IPython.display import clear_output
-import time
-
 #Main program:
 choice=0 #declaring the variable and initializing it with 0 value
 cat=0 #declaring the variable and initializing it with 0 value
